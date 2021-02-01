55e76cabb6dc4dc1183d880da893a9c509540113
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-01 13:05:24 +0100

    basic operations

--- js01.js
+++ js01.js
@@ -7,3 +7,23 @@ var price1 = 11;
 var price2 = 9;
 var total = price1 + price2;
 console.log("total price is " + total);
+
+var pi = 3.14;
+var person = "John Doe";
+var answer = 'Yes I am!';
+console.log(pi + " " + person + " " + answer);
+
+var a = 5 + 2 + 3;
+var aa = "5" + 2 + 3;
+var aaa = 2 + 3 + "5";
+console.log(a + " " + aa + " " + aaa);
+
+var b = 5;
+var bb = 2;
+var bbb = b * bb;
+var bbbb = b + bb;
+console.log(bbb + " " + bbbb);
+
+var txt1 = "what a very ";
+txt1 += "nice day";
+console.log(txt1);
